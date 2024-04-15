d39b8a7c550a7a2a4645b9ffd5522303d80a09ff
anhpham <anhpham77346@gmail.com>
2024-04-16 00:15:53 +0700

    test

--- frontend-admin/src/organisms/Sidebar.tsx
+++ frontend-admin/src/organisms/Sidebar.tsx
@@ -2,6 +2,11 @@ function Sidebar() {
     return (
         <div className="bg-[#1C2434] flex flex-col w-[250px] h-screen text-white p-4">
             {/* Header */}
+            <div className="h-[70px] flex justify-between items-center p-3">
+                <img className="h-[70px]" src="/icon.png" alt="" />
+                <p className="text-2xl font-bold">Order app</p>
+            </div>
+
             {/* Body */}
         </div>
     );
