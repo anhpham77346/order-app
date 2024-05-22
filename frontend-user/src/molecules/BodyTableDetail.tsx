453bf0ed0cb536faf54b6329aa132ed95629eb6b
anhpham <anhpham77346@gmail.com>
2024-05-22 16:06:38 +0700

    adasdasd

--- frontend-user/src/molecules/BodyTableDetail.tsx
+++ frontend-user/src/molecules/BodyTableDetail.tsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
 
 function BodyTableDetail() {
+    const hasJoinedTable = localStorage.getItem(`${import.meta.env.VITE_APP_API_URL}-table`);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // get hóa đơn
+            console.log(hasJoinedTable);
+            
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div className="h-[calc(100vh-80px)] overflow-y-auto px-6 py-3">
-            adasdasdasd
+            <div className="font-semibold text-[20px]">Hoá đơn của bạn</div>
         </div>
     );
 };
--- frontend-user/src/organisms/Sidebar.tsx
+++ frontend-user/src/organisms/Sidebar.tsx
@@ -8,6 +8,7 @@ function Sidebar() {
 
     const logout = () => {
         localStorage.removeItem(`${import.meta.env.VITE_APP_API_URL}-user`);
+        localStorage.removeItem(`${import.meta.env.VITE_APP_API_URL}-table`);
         navigate(routes.login);
     }
 
@@ -18,9 +19,14 @@ function Sidebar() {
     const goToMenu = () => {
         navigate(`${routes.menu}`);
     }
-    
+
     const goToTable = () => {
         navigate(`${routes.table}`);
+    }
+
+    const cancel = () => {
+        localStorage.removeItem(`${import.meta.env.VITE_APP_API_URL}-table`);
+        navigate(`${routes.home}`);
     }
 
     return (
@@ -56,7 +62,7 @@ function Sidebar() {
                     </>
                 ) : (
                     <>
-                        <div onClick={goToTable} className={`cursor-pointer flex gap-4 items-center px-6 py-3 rounded-md ${location.pathname === routes.table ? 'bg-slate-700' : 'hover:bg-slate-700'}`}>
+                        <div onClick={goToTable} className={`cursor-pointer flex gap-4 items-center px-6 py-3 rounded-md ${location.pathname === routes.table || location.pathname === routes.home ? 'bg-slate-700' : 'hover:bg-slate-700'}`}>
                             <div>
                                 <svg className="fill-current" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M15.7499 2.9812H14.2874V2.36245C14.2874 2.02495 14.0062 1.71558 13.6405 1.71558C13.2749 1.71558 12.9937 1.99683 12.9937 2.36245V2.9812H4.97803V2.36245C4.97803 2.02495 4.69678 1.71558 4.33115 1.71558C3.96553 1.71558 3.68428 1.99683 3.68428 2.36245V2.9812H2.2499C1.29365 2.9812 0.478027 3.7687 0.478027 4.75308V14.5406C0.478027 15.4968 1.26553 16.3125 2.2499 16.3125H15.7499C16.7062 16.3125 17.5218 15.525 17.5218 14.5406V4.72495C17.5218 3.7687 16.7062 2.9812 15.7499 2.9812ZM1.77178 8.21245H4.1624V10.9968H1.77178V8.21245ZM5.42803 8.21245H8.38115V10.9968H5.42803V8.21245ZM8.38115 12.2625V15.0187H5.42803V12.2625H8.38115ZM9.64678 12.2625H12.5999V15.0187H9.64678V12.2625ZM9.64678 10.9968V8.21245H12.5999V10.9968H9.64678ZM13.8374 8.21245H16.228V10.9968H13.8374V8.21245ZM2.2499 4.24683H3.7124V4.83745C3.7124 5.17495 3.99365 5.48433 4.35928 5.48433C4.7249 5.48433 5.00615 5.20308 5.00615 4.83745V4.24683H13.0499V4.83745C13.0499 5.17495 13.3312 5.48433 13.6968 5.48433C14.0624 5.48433 14.3437 5.20308 14.3437 4.83745V4.24683H15.7499C16.0312 4.24683 16.2562 4.47183 16.2562 4.75308V6.94683H1.77178V4.75308C1.77178 4.47183 1.96865 4.24683 2.2499 4.24683ZM1.77178 14.5125V12.2343H4.1624V14.9906H2.2499C1.96865 15.0187 1.77178 14.7937 1.77178 14.5125ZM15.7499 15.0187H13.8374V12.2625H16.228V14.5406C16.2562 14.7937 16.0312 15.0187 15.7499 15.0187Z" fill=""></path>
@@ -81,14 +87,27 @@ function Sidebar() {
                 )}
             </div>
 
-            <div onClick={logout} className="cursor-pointer flex gap-4 items-center px-6 py-3 hover:bg-slate-700 rounded-md">
-                <div>
-                    <svg fill="#ffffff" width="18px" height="18px" viewBox="0 0 32 32" id="Outlined" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Fill"> <path d="M25,2H16V4h9a1,1,0,0,1,1,1V27a1,1,0,0,1-1,1H16v2h9a3,3,0,0,0,3-3V5A3,3,0,0,0,25,2Z"></path> <path d="M21.58,17V15H7l4-4L9.58,9.55l-5,5a2,2,0,0,0,0,2.83l5,5L11,21,7,17Z"></path> </g> </g></svg>
+            {hasJoinedTable ? (
+                <>
+                    <div onClick={cancel} className="cursor-pointer flex gap-4 items-center px-6 py-3 hover:bg-slate-700 rounded-md">
+                        <div>
+                            <svg fill="#ffffff" width="18px" height="18px" viewBox="0 0 32 32" id="Outlined" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Fill"> <path d="M25,2H16V4h9a1,1,0,0,1,1,1V27a1,1,0,0,1-1,1H16v2h9a3,3,0,0,0,3-3V5A3,3,0,0,0,25,2Z"></path> <path d="M21.58,17V15H7l4-4L9.58,9.55l-5,5a2,2,0,0,0,0,2.83l5,5L11,21,7,17Z"></path> </g> </g></svg>
+                        </div>
+                        <div>
+                            <p>Thoát bàn</p>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div onClick={logout} className="cursor-pointer flex gap-4 items-center px-6 py-3 hover:bg-slate-700 rounded-md">
+                    <div>
+                        <svg fill="#ffffff" width="18px" height="18px" viewBox="0 0 32 32" id="Outlined" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Fill"> <path d="M25,2H16V4h9a1,1,0,0,1,1,1V27a1,1,0,0,1-1,1H16v2h9a3,3,0,0,0,3-3V5A3,3,0,0,0,25,2Z"></path> <path d="M21.58,17V15H7l4-4L9.58,9.55l-5,5a2,2,0,0,0,0,2.83l5,5L11,21,7,17Z"></path> </g> </g></svg>
+                    </div>
+                    <div>
+                        <p>Đăng xuất</p>
+                    </div>
                 </div>
-                <div>
-                    <p>Đăng xuất</p>
-                </div>
-            </div>
+            )}
         </div>
     );
 };
