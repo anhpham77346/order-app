eff57ba53eadfaa7628e5ac5ebc6e3fcd83d1ca7
anhpham <anhpham77346@gmail.com>
2024-05-22 20:12:40 +0700

    done

---
+++ frontend-admin/src/api/table/deleteTable.ts
@@ -0,0 +1,30 @@
+async function deleteTable(data: {
+    tableId: number
+}) {
+    const userDataString = localStorage.getItem(`${import.meta.env.VITE_APP_API_URL}-user`);
+
+    if (!userDataString) {
+        console.error('Không tìm thấy dữ liệu.');
+        return;
+    }
+
+    const userData = JSON.parse(userDataString);
+    const token = userData.token;
+
+    const res = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/table/delete`, {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (res.ok) {
+        return (await res.json());
+    } else {
+        throw Error('Failed!!!');
+    }
+}
+
+export default deleteTable;
